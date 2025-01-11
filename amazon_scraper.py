f6a9f84ac5c69348d413b787dcfccb8676e811b6
Nwaneri Ryan <ryanthedev04@gmail.com>
2025-01-11 07:30:25 +0100

    fixed none prices issue

--- amazon_scraper.py
+++ amazon_scraper.py
@@ -2,14 +2,14 @@ from playwright.sync_api import sync_playwright
 
 def download_amazon_pages(query, page_from=1, page_to=2, export_location='.'):
     with sync_playwright() as playwright:
-        browser = playwright.firefox.launch(headless=False, slow_mo=3000)
+        browser = playwright.firefox.launch(headless=False, slow_mo=1000)
         page = browser.new_page()
         for page_num in range(page_from, page_to+1):
             print('process',f'https://www.amazon.com/s?k={query}&page={page_num}')
             page.goto(f'https://www.amazon.com/s?k={query}&page={page_num}')
             page.wait_for_load_state('domcontentloaded')
 
-            with open(f'{export_location}/{query}_{page_num}.html', 'x', encoding='utf-8') as f:
+            with open(f'{export_location}/{query}_{page_num}.html', 'w', encoding='utf-8') as f:
                 f.write(page.content())
 
         browser.close()
--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from amazon_scraper import download_amazon_pages
 
-AmazonProduct = namedtuple('AmazonProduct', ['name','price','url'])
+AmazonProduct = namedtuple('AmazonProduct', ['name','price'])
+
+def extract_prop(product_element):
+    name = product_element.find('h2','a-size-medium a-spacing-none a-color-base a-text-normal').span.text
+    price = product_element.find('span','a-price').find('span','a-offscreen').text if product_element.find('span','a-price').find('span','a-offscreen').text else None
+    return AmazonProduct(name=name, price=price)
 
 if __name__ == '__main__':
     query = 'soundcore'
-    source_dir = Path('./exports')
+    source_dir = Path('./export')
     download_amazon_pages(query=query,page_from=1,page_to=3, export_location=source_dir)
 
     records = []
@@ -19,6 +24,17 @@ if __name__ == '__main__':
             soup = BeautifulSoup(f, 'html.parser')
         print(f'Processing file {html_file}')
         result_list = soup.find('div','s-main-slot s-result-list s-search-results sg-row')
-        products = result_list.find_all('div',{
-            'role':True
+        products = result_list.find_all('div',attrs={
+            'role':'listitem',
+            'data-component-type':'s-search-result'
         })
+        priced_products = []
+        for product in products:
+            if product.find('span','a-price') == None:
+                continue
+            #priced_products.append(product.find('span','a-price').find('span','a-offscreen').text)
+            priced_products.append(product)
+        for product in priced_products:
+            records.append(extract_prop(product))    
+
+    print(len(records))
